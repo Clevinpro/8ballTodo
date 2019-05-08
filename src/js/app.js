cee2046778fb97d512938eb6066eca03ba1b8435
Sinas66 <Pashkomania@gmail.com>
2019-05-08 15:32:00 +0300

    genarateId fn

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-const notes = [
+import { genarateId } from "./utils/helper";
+
+export const notes = [
   {
     id: 1,
     title: "First",
@@ -29,22 +31,36 @@ const notes = [
   }
 ];
 
+notes.length;
 class App {
   constructor() {
     this._notes = notes;
     this.refs = {};
     this.refs.noteList = document.querySelector(".main__card-todo");
+    this.refs.start = document.querySelector(`.main__card__start`);
+    this.refs.inprogress = document.querySelector(`.main__card__progress`);
+    this.refs.completed = document.querySelector(`.main__card__completed`);
+
+    this.renderNodesList = this.renderNodesList.bind(this);
   }
 
+  // genarateId() {}
+
+  addTodo() {}
+
+  deleteTodo() {}
+
+  editTodo() {}
+
   createNoteContent({ title, body }) {
+    // console.log(document);
     const div = document.createElement("DIV");
     const h2 = document.createElement("H2");
     const p = document.createElement("P");
-
-    div.className = "note__content";
+    div.className = "main__card-todo";
     h2.textContent = title;
-    h2.className = "note__title";
-    p.className = "note__body";
+    h2.className = "main__card-title";
+    p.className = "main__card-body";
     p.textContent = body;
     div.appendChild(h2);
     div.appendChild(p);
@@ -53,17 +69,20 @@ class App {
   }
 
   renderNodesList() {
-    const start = document.querySelector(`main__card__start`);
-    const inprogress = document.querySelector(`main__card__progress`);
-    const completed = document.querySelector(`main__card__completed`);
     this._notes.forEach(el => {
       if (el.inProgress) {
-        inprogress.append(createElement(el));
+        this.refs.inprogress.append(this.createNoteContent(el));
       } else if (el.complted) {
-        completed.append(createElement(el));
+        this.refs.completed.append(this.createNoteContent(el));
       } else {
-        start.append(createElement(el));
+        this.refs.start.append(this.createNoteContent(el));
       }
     });
   }
 }
+
+
+const app = new App();
+app.renderNodesList();
+
+genarateId(notes)
