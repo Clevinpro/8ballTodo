ee1db0c04d5ae81d53a6f8c415890b92dad49375
Valerii Kovalenko <kovalenko.vv.smartagency@gmail.com>
2019-05-13 11:14:06 +0300

    preloader added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import "./sass/main.scss";
 import app from "./js/app";
 import { loadNotes } from './js/utils/api';
 
+// window.onload = () => {
+//   setTimeout(() => 
+//     document.querySelector('.preload').style.display = 'none', 1500
+//   )
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.querySelector('.preload').style.display = 'none';
+        document.querySelector('.app').style.display = 'block';
+    }, 1500
+    )
+})
 
 const todo = new app();
 
